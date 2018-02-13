f5ca320859664b584ecedadbd707847da319f7d1
SuzukiMasashi <suzuki_masasi@nexway.co.jp>
2018-02-13 19:31:58 +0900

    :bug: Fix spec

--- test/resources/roman_numeral_test.rb
+++ test/resources/roman_numeral_test.rb
@@ -4,19 +4,19 @@ require File.dirname(__FILE__) + '/../test_helper'
 require_relative '../../app/resources/roman_numeral'
 
 class RomanNumeralTest < ActiveSupport::TestCase
-  test '#to_s, ⅩⅡ' do
+  test '#to_s, ⅩⅠⅠ' do
     roman_numeral = RomanNumeral.new(numeral: 12)
-    assert_equal 'ⅩⅡ', roman_numeral.to_s
+    assert_equal 'ⅩⅠⅠ', roman_numeral.to_s
   end
 
-  test '#to_s, ⅩⅩⅣ' do
+  test '式展開, ⅩⅩⅠⅤ' do
     roman_numeral = RomanNumeral.new(numeral: 24)
-    assert_equal 'ⅩⅩⅣ', roman_numeral.to_s
+    assert_equal 'ⅩⅩⅠⅤ', "#{roman_numeral}"
   end
 
-  test '#+, ⅩⅩⅣ' do
+  test '#+, ⅩⅩⅠⅤ' do
     roman_numeral1 = RomanNumeral.new(numeral: 12)
     roman_numeral2 = RomanNumeral.new(numeral: 12)
-    assert_equal 'ⅩⅩⅣ', "#{roman_numeral1 + roman_numeral2}"
+    assert_equal 'ⅩⅩⅠⅤ', "#{roman_numeral1 + roman_numeral2}"
   end
 end
