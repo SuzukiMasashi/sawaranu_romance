6a2638699a2b30cca7960621640b91864e44767a
ubirei <u_birei@nexway.co.jp>
2018-02-21 13:08:54 +0900

    implement to_s method

--- app/resources/roman_numeral.rb
+++ app/resources/roman_numeral.rb
@@ -9,6 +9,59 @@ class RomanNumeral < Dry::Struct
 
   attribute :numeral, Types::Coercible::Int
 
+  ROMAN_DICTIONARY = {
+        1  => 'I',
+        2  => 'II',
+        3  => 'III',
+        4  => 'IV',
+        5  => 'V',
+        6  => 'VI',
+        7  => 'VII',
+        8  => 'VIII',
+        9  => 'IX',
+       10  => 'X',
+       20  => 'XX',
+       30  => 'XXX',
+       40  => 'XL',
+       50  => 'L',
+       60  => 'LX',
+       70  => 'LXX',
+       80  => 'LXXX',
+       90  => 'XC',
+      100  => 'C',
+      200  => 'CC',
+      300  => 'CCC',
+      400  => 'CD',
+      500  => 'D',
+      600  => 'DC',
+      700  => 'DCC',
+      800  => 'DCCC',
+      900  => 'CM',
+    1_000  => 'M',
+    2_000  => 'MM',
+    3_000  => 'MMM'
+  }
+
   def to_s
+    return 'error' unless numeral.between?(1, 3999)
+
+    roman_str = ''
+    balance   = numeral
+
+    [1000, 100, 10, 1].each do |unit|
+      div = balance.div(unit)
+      if div > 0 && balance > 0
+        roman_str += ROMAN_DICTIONARY[div * unit]
+        balance   -= div * unit
+      end
+    end
+
+    roman_str
+  end
+
+  def +(other)
+    return 'error' unless other.is_a?(RomanNumeral)
+
+    RomanNumeral.new(numeral: numeral + other.numeral).to_s
   end
 end
--- test/resources/roman_numeral_test.rb
+++ test/resources/roman_numeral_test.rb
@@ -4,19 +4,124 @@ require File.dirname(__FILE__) + '/../test_helper'
 require_relative '../../app/resources/roman_numeral'
 
 class RomanNumeralTest < ActiveSupport::TestCase
-  test '#to_s' do
+  test '#to_s_1' do
+    roman_numeral = RomanNumeral.new(numeral: 1)
+    assert { roman_numeral.to_s == 'I' }
+  end
+
+  test '#to_s_2' do
+    roman_numeral = RomanNumeral.new(numeral: 2)
+    assert { roman_numeral.to_s == 'II' }
+  end
+
+  test '#to_s_3' do
+    roman_numeral = RomanNumeral.new(numeral: 3)
+    assert { roman_numeral.to_s == 'III' }
+  end
+
+  test '#to_s_4' do
+    roman_numeral = RomanNumeral.new(numeral: 4)
+    assert { roman_numeral.to_s == 'IV' }
+  end
+
+  test '#to_s_5' do
+    roman_numeral = RomanNumeral.new(numeral: 5)
+    assert { roman_numeral.to_s == 'V' }
+  end
+
+  test '#to_s_6' do
+    roman_numeral = RomanNumeral.new(numeral: 6)
+    assert { roman_numeral.to_s == 'VI' }
+  end
+
+  test '#to_s_7' do
+    roman_numeral = RomanNumeral.new(numeral: 7)
+    assert { roman_numeral.to_s == 'VII' }
+  end
+
+  test '#to_s_8' do
+    roman_numeral = RomanNumeral.new(numeral: 8)
+    assert { roman_numeral.to_s == 'VIII' }
+  end
+
+  test '#to_s_9' do
+    roman_numeral = RomanNumeral.new(numeral: 9)
+    assert { roman_numeral.to_s == 'IX' }
+  end
+
+  test '#to_s_10' do
+    roman_numeral = RomanNumeral.new(numeral: 10)
+    assert { roman_numeral.to_s == 'X' }
+  end
+
+  test '#to_s_12' do
     roman_numeral = RomanNumeral.new(numeral: 12)
-    assert { roman_numeral.to_s == 'ⅩⅠⅠ' }
+    assert { roman_numeral.to_s == 'XII' }
+  end
+
+  test '#to_s_42' do
+    roman_numeral = RomanNumeral.new(numeral: 42)
+    assert { roman_numeral.to_s == 'XLII' }
+  end
+
+  test '#to_s_49' do
+    roman_numeral = RomanNumeral.new(numeral: 49)
+    assert { roman_numeral.to_s == 'XLIX' }
+  end
+
+  test '#to_s_89' do
+    roman_numeral = RomanNumeral.new(numeral: 89)
+    assert { roman_numeral.to_s == 'LXXXIX' }
+  end
+
+  test '#to_s_99' do
+    roman_numeral = RomanNumeral.new(numeral: 99)
+    assert { roman_numeral.to_s == 'XCIX' }
+  end
+
+  test '#to_s_100' do
+    roman_numeral = RomanNumeral.new(numeral: 100)
+    assert { roman_numeral.to_s == 'C' }
+  end
+
+  test '#to_s_299' do
+    roman_numeral = RomanNumeral.new(numeral: 299)
+    assert { roman_numeral.to_s == 'CCXCIX' }
+  end
+
+  test '#to_s_493' do
+    roman_numeral = RomanNumeral.new(numeral: 493)
+    assert { roman_numeral.to_s == 'CDXCIII' }
+  end
+
+  test '#to_s_1000' do
+    roman_numeral = RomanNumeral.new(numeral: 1000)
+    assert { roman_numeral.to_s == 'M' }
+  end
+
+  test '#to_s_1960' do
+    roman_numeral = RomanNumeral.new(numeral: 1960)
+    assert { roman_numeral.to_s == 'MCMLX' }
+  end
+
+  test '#to_s_2018' do
+    roman_numeral = RomanNumeral.new(numeral: 2018)
+    assert { roman_numeral.to_s == 'MMXVIII' }
+  end
+
+  test '#to_s_3999' do
+    roman_numeral = RomanNumeral.new(numeral: 3999)
+    assert { roman_numeral.to_s == 'MMMCMXCIX' }
   end
 
   test '式展開' do
     roman_numeral = RomanNumeral.new(numeral: 24)
-    assert { "#{roman_numeral}" == 'ⅩⅩⅠⅤ' }
+    assert { "#{roman_numeral}" == 'XXIV' }
   end
 
   test '#+' do
     roman_numeral1 = RomanNumeral.new(numeral: 12)
     roman_numeral2 = RomanNumeral.new(numeral: 12)
-    assert { "#{roman_numeral1 + roman_numeral2}" == 'ⅩⅩⅠⅤ' }
+    assert { "#{roman_numeral1 + roman_numeral2}" == 'XXIV' }
   end
 end
